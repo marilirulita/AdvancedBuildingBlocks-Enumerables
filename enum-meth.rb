a4d01c0cf86ba94aace43e6220e220e693d61e9b
marysol <marilirulita@gmail.com>
2021-01-21 22:34:06 -0800

    Add last change of module

--- enum-meth.rb
+++ enum-meth.rb
@@ -1,6 +1,4 @@
 module Enumerable
-  #Remember yield and the call method.
-
   #my_each, a method that is identical to #each but (obviously) does not use #each. You'll need to remember the yield statement. Make sure it returns the same thing as #each as well.
   def my_each
     a = 0
@@ -157,15 +155,10 @@ module Enumerable
     end
     a
   end
-
-  #Test my_inject by creating a method called #multiply_els which multiplies all the elements of the array together by using #my_inject, e.g. multiply_els([2,4,5]) #=> 40
-  def multiply_els(m)
-    m.my_inject {|x, l| x * l}
-  end
-  puts multiply_els([2, 4, 5])
-
-  #Modify my_map method to take a proc instead.
-
-
-  #Modify my_map method to take either a proc or a block. It won't be necessary to apply both a proc and a block in the same #my_map call since you could get the same effect by chaining together one #my_map call with the block and one with the proc. This approach is also clearer, since the user doesn't have to remember whether the proc or block will be run first. So if both a proc and a block are given, only execute the proc.
 end
+
+#Test my_inject by creating a method called #multiply_els which multiplies all the elements of the array together by using #my_inject, e.g. multiply_els([2,4,5]) #=> 40
+def multiply_els(m)
+  m.my_inject {|x, l| x * l}
+end
+puts multiply_els([2, 4, 5])
