aa5c1f609f620b940b45753269a603ff732f0bc7
marysol <marilirulita@gmail.com>
2021-02-16 14:17:58 -0800

    Add test for my each method

---
+++ spec/enumerable_spec.rb
@@ -0,0 +1,11 @@
+require './enum_meth'
+extend Enumerable
+
+
+describe "#my_each" do
+let(:array) {["Mar", "Dany", "Sofia"]}
+  it "return each element" do
+    expect(array.my_each).to include("Dany")
+  end
+end
+
