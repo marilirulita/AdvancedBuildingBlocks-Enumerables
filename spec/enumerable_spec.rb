8bd476ae41fb64ec52813d287bc1dbed4b1d9a00
tj-ubuntu <abongsjoel@gmail.com>
2021-02-17 22:17:08 +0100

    Make Enumerables a describe method

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -1,36 +1,39 @@
 require './enum_meth'
-extend Enumerable
 
+describe Enumerable do
+  let(:array) {["Mar", "Dany", "Sofia"]}
 
-describe "#my_each" do
-let(:array) {["Mar", "Dany", "Sofia"]}
-  it "return each element given in the array" do
-    expect(array.my_each).to include("Dany")
+  describe "#my_each" do
+    it "return each element given in the array" do
+      expect(array.my_each).to include("Dany")
+    end
+  
+    it "if any block passed in returns an enumerable" do
+      expect(array.my_each) == (array.to_enum)
+    end
   end
-
-  it "if any block passed in returns an enumerable" do
-    expect(array.my_each) == (array.to_enum)
+  
+  describe "#my_each_with_index" do
+    it "if a block is passed in returns the array elements given" do
+      expect(array.my_each_with_index {|e, i| i if e == "Dany"}).to eql(["Mar", "Dany", "Sofia"])
+    end
+  
+    it "if any block passed in returns an enumerable" do
+      expect(array.my_each_with_index) == (array.to_enum)
+    end
+  end
+  
+  describe "#my_select" do
+    it "returns an array without element select" do
+      expect(array.my_select {|e| e != "Dany"}).to eql(["Mar", "Sofia"])
+    end
+  
+    it "returns an array that does not include specific name" do
+      expect(array.my_select {|e| e != "Mar" }).not_to include("Mar")
+    end
+  end
+  
+  describe "#my_all?" do
   end
 end
 
-describe "#my_each_with_index" do
-let(:array) {["Mar", "Dany", "Sofia"]}
-  it "if a block is passed in returns the array elements given" do
-    expect(array.my_each_with_index {|e, i| i if e == "Dany"}).to eql(["Mar", "Dany", "Sofia"])
-  end
-
-  it "if any block passed in returns an enumerable" do
-    expect(array.my_each_with_index) == (array.to_enum)
-  end
-end
-
-describe "#my_select" do
-let(:array) {["Mar", "Dany", "Sofia"]}
-  it "returns an array without element select" do
-    expect(array.my_select {|e| e != "Dany"}).to eql(["Mar", "Sofia"])
-  end
-
-  it "returns an array that does not include specific name" do
-    expect(array.my_select {|e| e != "Mar" }).not_to include("Mar")
-  end
-end
